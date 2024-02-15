c2038392ef732d9ecbe55ddae0193366d94d4994
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-16 00:20:10 +0530

    Added  Forgot Password Component

---
+++ frontend/src/pages/auth/Forgot.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styles from "./auth.module.scss";
+import Card from "../../components/card/Card";
+import { AiOutlineMail } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/passwordInput/PasswordInput";
+const Forgot = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChange = (e) => {};
+  const loginUser = () => {};
+
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className="--flex-center">
+            <AiOutlineMail size={35} color="#999" />
+          </div>
+          <h2>Forgot Password</h2>
+
+          <form onSubmit={loginUser}>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+              value={email}
+              onChange={handleInputChange}
+            />
+
+            <button type="submit" className="--btn --btn-primary --btn-block">
+              Get Reset Email
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/">- Home</Link>
+              </p>
+              <p>
+                <Link to="/login">- Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default Forgot;
