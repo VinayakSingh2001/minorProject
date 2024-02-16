e7d4f66644d09f9032089eef60724844795a789e
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-16 13:05:48 +0530

    Added User Table

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./UserList.scss";
+import { FaTrashAlt } from "react-icons/fa";
 import PageMenu from "../../components/pageMenu/PageMenu";
 import UserStats from "../../components/userStats/UserStats";
+import Search from "../../components/search/Search";
 
 const UserList = () => {
   return (
@@ -9,6 +11,46 @@ const UserList = () => {
       <div className="container">
         <PageMenu />
         <UserStats />
+        <div className="user-list">
+          <div className="table">
+            <div className="--flex-between">
+              <span>
+                <h3>All Users</h3>
+              </span>
+              <span>
+                <Search />
+              </span>
+            </div>
+            {/* Table */}
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Change Role</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Vinayak</td>
+                  <td>vinayak@test.com</td>
+                  <td>admin</td>
+                  <td>Change Role</td>
+                  <td>
+                    <span>
+                      {" "}
+                      <FaTrashAlt size={20} color="red" />
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </section>
   );
