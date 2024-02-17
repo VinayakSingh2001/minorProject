55ecf6f5df4207449a8bb376520b2680b7730f14
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-17 14:26:16 +0530

    Created User Model

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,18 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000/", "https://auth-app.vercel.app/"],
+    credentials: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("home page");
 });
