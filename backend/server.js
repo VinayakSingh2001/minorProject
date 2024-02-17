d866b3b7cad3cbd1dedbd3facc16556e2ab58d84
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-17 13:37:20 +0530

    Created Server and Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
-import React from "react";
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const server = () => {
-  return <div></div>;
-};
+const app = express();
 
-export default server;
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
