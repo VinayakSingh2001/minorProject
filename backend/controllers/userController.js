e29ff9d38ba65074f11e106d8241b0c9d676d52e
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-19 03:44:30 +0530

    Setup Login Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
     name,
     email,
     password,
-    userAgent
+    userAgent,
   });
 
   // Generate Token
@@ -78,6 +78,69 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  //validation
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill Email and Password.");
+  }
+
+  //check if user exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, Please register.");
+  }
+
+  //when we were saving the password we hashed it via bcrypt, but bcrypt has a way to compare the passwords
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+  if (!passwordIsCorrect) {
+    res.status(400);
+    throw new Error("Invalid email or password.");
+  }
+  //TRIGGER 2 FACTOR AUTH FOR unknown UserAgents
+  //now if user exists and password is correct, the next thing we need to check
+  //if that user's device is authorized or not
+  //we login the user by sending a token(a jsonwebtoken to the user's client or browser)
+
+  //Generate token
+  const token = generateToken(user._id);
+
+  if (user && passwordIsCorrect) {
+    //as user exists and password is correct, then we login the user ,
+    //HOW DO WE LOGIN--> we just sendin the http cookie to the frontend that will contain our token
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      sameSite: "none",
+      secure: true,
+    });
+    //now we also want to send some data about the user to the frontend
+    //so we will send it in json
+    const { _id, name, email, phone, bio, photo, role, isVerified } = user;
+
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      phone,
+      bio,
+      photo,
+      role,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(500);
+    throw new Error("Something went wrong,Please try again.");
+  }
+});
+
 module.exports = {
   registerUser,
+  loginUser,
 };
